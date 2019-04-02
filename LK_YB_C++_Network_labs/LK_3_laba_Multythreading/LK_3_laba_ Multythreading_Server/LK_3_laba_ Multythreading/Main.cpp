dbaa05a69de709541a5391024828fe206092c46c
Artyom <artem.tsvirko@mail.ru>
2019-04-02 21:25:42 +0300

    added LK_3_laba_Multythreading

---
+++ LK_YB_C++_Network_labs/LK_3_laba_Multythreading/LK_3_laba_ Multythreading_Server/LK_3_laba_ Multythreading/Main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <WinSock2.h>
+// do not forget to link Winsock lib in prj settings
+using namespace std;
+
+int checkLength(const char* buf, int size) {
+	int length = 0;
+	for (int i = 0; i < size; i++) {
+		if (buf[i] == '\0') {
+			return length;
+		}
+		else
+			length++;
+	}
+	return -1;
+}
+
+DWORD WINAPI Payload(
+	LPVOID lpParameter
+)
+{
+	SOCKET sock = (SOCKET)lpParameter;
+
+	while (true) {
+		char incomingString[64];
+		recv(sock, incomingString, sizeof(incomingString), NULL);
+		if (strcmp(incomingString, "!quit") == 0) {
+			cout << "done with another client!\n";
+			return 0;
+		}
+
+		int length = checkLength(incomingString, sizeof(incomingString));
+		
+		send(sock, reinterpret_cast<char*>(&length), sizeof(int), NULL);
+		
+		if (length > 15) {
+			char outcomingString[64];
+			for (int i = 0; i < sizeof(outcomingString); i++) {
+				if (incomingString[i] == ' ') {
+					memcpy(outcomingString, incomingString, i);
+					outcomingString[i] = '\0';
+					send(sock, outcomingString, sizeof(outcomingString), NULL);
+					break;
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	WORD wVersionRequested;
+	WSADATA wsadata;
+	wVersionRequested = MAKEWORD(2, 2);
+	if (WSAStartup(wVersionRequested, &wsadata) != NO_ERROR) {
+		cout << "init error\n";
+		return 1;
+	}
+
+	SOCKET ls = socket(AF_INET, SOCK_STREAM, 0);
+	if (ls == INVALID_SOCKET) {
+		cout << "invalid socket\n";
+		WSACleanup();
+		return 2;
+	}
+
+	sockaddr_in local_addr;
+	local_addr.sin_family = AF_INET;
+	local_addr.sin_port = htons(50001);
+	local_addr.sin_addr.s_addr = 0;
+
+	if (bind(ls, (sockaddr *)&local_addr, sizeof(local_addr)) == SOCKET_ERROR) {
+		cout<<"bind err\n";
+		closesocket(ls);
+		WSACleanup();
+		return 3;
+	}
+
+	if (listen(ls, SOMAXCONN) == SOCKET_ERROR) {
+		cout << "listen error\n";
+		closesocket(ls);
+		WSACleanup();
+		return 4;
+	}
+
+	SOCKADDR_IN addrClient;
+	int addrlen = sizeof(SOCKADDR);
+
+	while (true) {
+		SOCKET s = accept(ls, (SOCKADDR*)&addrClient, &addrlen);
+		if (s == INVALID_SOCKET) break;
+
+		DWORD dwThread;
+		HANDLE hThread = CreateThread(NULL,
+										0,
+										Payload,
+										(LPVOID)s,
+										0,
+										&dwThread);
+
+		if (hThread == NULL) {
+			cout << "thread creation failed!\n";
+			closesocket(s);
+			Sleep(60000);			// server is tired:)
+		} else {
+			cout << "new client successfully connected!\n";
+			CloseHandle(hThread);	// we do not need it anymore
+		}
+	}
+
+	closesocket(ls);
+	WSACleanup();
+	return 0;
+}
