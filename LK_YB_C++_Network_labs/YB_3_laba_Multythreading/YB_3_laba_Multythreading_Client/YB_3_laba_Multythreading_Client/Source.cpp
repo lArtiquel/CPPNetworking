336d207f461633238f20520ccaa2c5ced5bece33
Artyom <artem.tsvirko@mail.ru>
2019-04-04 23:43:05 +0300

    YB_3_laba_Multythreading added

---
+++ LK_YB_C++_Network_labs/YB_3_laba_Multythreading/YB_3_laba_Multythreading_Client/YB_3_laba_Multythreading_Client/Source.cpp
@@ -0,0 +1,62 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <iostream>
+// link ws2_32 in prj settings!
+using namespace std;
+
+int main()
+{
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR) {
+		cout << "WSAStartup failed with error\n";
+		return 1;
+	}
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET) {
+		cout << "Error at socket\n";
+		WSACleanup();
+		return 2;
+	}
+
+	sockaddr_in addrServer;
+	addrServer.sin_family = AF_INET;
+	addrServer.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addrServer.sin_port = htons(50001);
+
+	iResult = connect(sock, (SOCKADDR*)&addrServer, sizeof(addrServer));
+	if (iResult == SOCKET_ERROR) {
+		closesocket(sock);
+		cout << "Unable to connect to server\n";
+		WSACleanup();
+		return 3;
+	}
+
+	while (1)
+	{
+		char outcomingString[64];
+		cout << "enter another string: ";
+		cin.getline(outcomingString, sizeof(outcomingString));
+
+		send(sock, outcomingString, sizeof(outcomingString), NULL);
+		if (strcmp(outcomingString, "!quit") == 0) {
+			break;
+		}
+
+		int length = 0;
+		recv(sock, reinterpret_cast<char*>(&length), sizeof(int), NULL);
+		cout << "server sended: " << length << " - length of sended string\n";
+
+		if (length % 2) {
+			recv(sock, outcomingString, sizeof(outcomingString), NULL);
+			cout << "another server's response: \"";
+			cout << outcomingString << "\" --- string with removed char in the middle\n";
+		}
+	}
+
+	closesocket(sock);
+	WSACleanup();
+	system("pause");
+	return 0;
+}
